44e64924aa405c6dd0db52d3058ded448745f3a3
shail20IT136 <88127142+shail1012@users.noreply.github.com>
2022-11-05 22:23:47 +0530

    Add files via upload

---
+++ src/components/Details.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
+
+const Details = () => {
+
+    const [logindata, setLoginData] = useState([]);
+
+
+    const history = useNavigate();
+
+    const [show, setShow] = useState(false);
+
+    var todayDate = new Date().toISOString().slice(0, 10);
+  
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const Birthday = () => {
+        const getuser = localStorage.getItem("user_login");
+        if (getuser && getuser.length) {
+            const user = JSON.parse(getuser);
+         
+            setLoginData(user);
+
+
+            const userbirth = logindata.map((el, k) => {
+                return el.date === todayDate
+            });
+
+            if (userbirth) {
+                setTimeout(() => {
+                    console.log("ok");
+                    handleShow();
+                }, 3000)
+            }
+        }
+    }
+
+    const userlogout = ()=>{
+        localStorage.removeItem("user_login")
+        history("/");
+    }
+
+    useEffect(() => {
+        Birthday();
+    }, [])
+
+    return (
+        <>
+            <div>
+        <h1>Upload File that has to be secured</h1>
+        <input type="file" onChange={(e)=>this.upload(e)} name="img" />
+      </div>
+        </>
+    )
+}
+
+export default Details
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
